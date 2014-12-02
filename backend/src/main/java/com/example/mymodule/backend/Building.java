6ecdf60eb58a172ddcacf91799c3ad5b96d6c06a
Siddhesh Gadkari <sgadkari6@gatech.edu>
2014-12-02 17:38:01 -0500

    Added all events in backend to scheduler (Test)

---
+++ backend/src/main/java/com/example/mymodule/backend/Building.java
@@ -0,0 +1,47 @@
+package edu.gatech.cs8803.team4.building;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class Building {
+private long id;
+private String location;
+public Building(long buildingid){
+	id=buildingid;
+	String query="https://1-dot-rlr-gtnow-backend.appspot.com/_ah/api/building/v1/building/"+id;
+	String USER_AGENT = "Chrome/38.0";
+	try{
+		URL obj = new URL(query);
+		HttpURLConnection con;
+		con = (HttpURLConnection) obj.openConnection();
+		// optional default is GET
+		con.setRequestMethod("GET");
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		String jsonstring = new String();
+			
+		while ((inputLine = in.readLine()) != null) {
+			jsonstring+=inputLine+"\n";
+		}
+		
+		JSONObject building = (JSONObject) new JSONParser().parse(jsonstring);
+		location=building.get("latitude")+","+building.get("longitude");
+	}
+	catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	};
+}
+
+public String getlocation(){
+	return location;
+}
+
+}
--- backend/src/main/java/com/example/mymodule/backend/Check.java
+++ backend/src/main/java/com/example/mymodule/backend/Check.java
@@ -5,6 +5,10 @@ import static com.google.appengine.api.taskqueue.TaskOptions.Builder.withUrl;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,9 +20,15 @@ import org.joda.time.Period;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.appengine.api.taskqueue.Queue;
 import com.google.appengine.api.taskqueue.QueueFactory;
+
+import com.example.mymodule.backend.Building;
 
 public class Check extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -30,11 +40,48 @@ public class Check extends HttpServlet {
 			ArrayList<Event> eventDB = new ArrayList<Event>();
 			DateTimeZone zone = DateTimeZone.forID("US/Eastern");
 			//userName, eventName, longitude, latitude, startTime
-			DateTimeFormatter df = DateTimeFormat.forPattern("yyyy/MM/dd HH:mm:ss").withZone(zone);
-			eventDB.add(new Event("user1", "class1", "88.34, 88.56", df.parseDateTime("2014/11/29 23:51:00")));		
-			eventDB.add(new Event("user2", "class3",  "88.34, 88.56", df.parseDateTime("2014/11/29 23:52:00")));
-			eventDB.add(new Event("user3", "class2", "88.34, 88.56", df.parseDateTime("2014/11/29 23:51:00")));		
-			eventDB.add(new Event("user1", "class3",  "88.34, 88.56", df.parseDateTime("2014/11/29 23:52:00")));
+			DateTimeFormatter df = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss'.'ZZZ'Z'").withZone(zone);
+
+        URL obj;
+        String query = "https://1-dot-rlr-gtnow-backend.appspot.com/_ah/api/event/v1/event";
+        String USER_AGENT="Chrome/38.0";
+        try {
+            obj = new URL(query);
+            HttpURLConnection con;
+            con = (HttpURLConnection) obj.openConnection();
+            // optional default is GET
+            con.setRequestMethod("GET");
+            //add request header
+            con.setRequestProperty("User-Agent", USER_AGENT);
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            String jsonstring = new String();
+
+            while ((inputLine = in.readLine()) != null) {
+                jsonstring+=inputLine+"\n";
+            }
+
+            JSONObject json = (JSONObject) new JSONParser().parse(jsonstring);
+            in.close();
+            json.remove("etag");
+            json.remove("kind");
+            for(int i=0;i<((JSONArray) json.get("items")).size();i++){
+                JSONObject j = (JSONObject) ((JSONArray) json.get("items")).get(i);
+                Building b = new Building((long) j.get("buildingID"));
+                eventDB.add(new Event((String) j.get("gtid"), (String) j.get("name"), b.getlocation(), df.parseDateTime((String) j.get("startTime"))));
+            }
+
+
+        }
+        catch (Exception e1){
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+
+			//eventDB.add(new Event("user1", "class1", "88.34, 88.56", df.parseDateTime("2014/11/29 23:51:00")));
+			//eventDB.add(new Event("user2", "class3",  "88.34, 88.56", df.parseDateTime("2014/11/29 23:52:00")));
+			//eventDB.add(new Event("user3", "class2", "88.34, 88.56", df.parseDateTime("2014/11/29 23:51:00")));
+			//eventDB.add(new Event("user1", "class3",  "88.34, 88.56", df.parseDateTime("2014/11/29 23:52:00")));
 			
 			//Add events for this day
 			for(Event e: eventDB) {		
